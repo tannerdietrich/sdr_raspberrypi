dc4c3f25dc307095bdbed85beab030bcba43bb8e
Tanner Dietrich <tanner.dietrich@healthcatalyst.com>
2023-12-31 12:13:34 -0700

    Initial commit of installation.

---
+++ rtl_sdr_drivers.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# print command in shell script and wait for confirmation before executing
+trap 'read -p "run: $BASH_COMMAND "' DEBUG
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y build-essential cmake libfftw3-dev libglfw3-dev libglew-dev libvolk2-dev libzstd-dev libsoapysdr-dev libairspyhf-dev libairspy-dev libiio-dev libad9361-dev librtaudio-dev libhackrf-dev librtlsdr-dev libbladerf-dev liblimesuite-dev p7zip-full wget libusb-1.0-0-dev
